b1c27bc1b3554010507f9c0ff0d41ae89960e496
Arijit RC <37416690+arc1234567@users.noreply.github.com>
2020-06-20 14:58:13 +0530

    Add files via upload

---
+++ mo.py
@@ -0,0 +1,143 @@
+# Core Pkgs
+import streamlit as st 
+
+# EDA Pkgs
+import pandas as pd 
+import numpy as np 
+
+
+# Data Viz Pkg
+import matplotlib.pyplot as plt 
+import matplotlib
+matplotlib.use("Agg")
+import seaborn as sns 
+from sklearn import preprocessing 
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+scaler1 = StandardScaler()
+
+
+
+def main():
+    """Semi Automated ML App with Streamlit """
+    html_temp = """
+    <div style="background-color:blue;padding:10px">
+    <h2 style="color:white;text-align:center;">Automated Data Analysis & Visualization! </h2>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+
+    activities = ["EDA","Plots","Pre-Processing"]    
+    choice = st.sidebar.selectbox("Select Activities",activities)
+
+    if choice == 'EDA':
+        st.subheader("Exploratory Data Analysis")
+
+        data = st.file_uploader("Upload a Dataset", type=["csv", "txt"])
+        if data is not None:
+            df = pd.read_csv(data)
+            st.dataframe(df.head())
+            if st.checkbox("Show Shape"):
+                st.write(df.shape)
+
+            if st.checkbox("Show Columns"):
+                all_columns = df.columns.to_list()
+                st.write(all_columns)
+
+            if st.checkbox("Summary"):
+                st.write(df.describe())
+
+            if st.checkbox("Show Selected Columns"):
+                selected_columns = st.multiselect("Select Columns",all_columns)
+                new_df = df[selected_columns]
+                st.dataframe(new_df)
+
+            if st.checkbox("Show Value Counts"):
+                st.write(df.iloc[:,-1].value_counts())
+            if st.checkbox("Show Missing Value Counts"):
+                st.write(df.isnull().sum())
+
+            if st.checkbox("Correlation Plot(Matplotlib)"):
+                plt.matshow(df.corr())
+                st.pyplot()
+
+            if st.checkbox("Correlation Plot(Seaborn)"):
+                st.write(sns.heatmap(df.corr(),annot=True))
+                st.pyplot()
+
+
+            if st.checkbox("Pie Plot"):
+                all_columns = df.columns.to_list()
+                column_to_plot = st.selectbox("Select 1 Column",all_columns)
+                pie_plot = df[column_to_plot].value_counts().plot.pie(autopct="%1.1f%%")
+                st.write(pie_plot)
+                st.pyplot()
+
+
+
+    elif choice == 'Plots':
+        st.subheader("Data Visualization")
+        data = st.file_uploader("Upload a Dataset", type=["csv", "txt", "xlsx"])
+        if data is not None:
+            df = pd.read_csv(data)
+            st.dataframe(df.head())
+
+
+            if st.checkbox("Show Value Counts"):
+                st.write(df.iloc[:,-1].value_counts().plot(kind='bar'))
+                st.pyplot() 
+            # Customizable Plot
+
+            all_columns_names = df.columns.to_list()
+            type_of_plot = st.selectbox("Select Type of Plot",["area","bar","line","hist","box","kde"])
+            selected_columns_names = st.multiselect("Select Columns To Plot",all_columns_names)
+
+            if st.button("Generate Plot"):
+                st.success("Generating Customizable Plot of {} for {}".format(type_of_plot,selected_columns_names))
+
+                # Plot By Streamlit
+                if type_of_plot == 'area':
+                    cust_data = df[selected_columns_names]
+                    st.area_chart(cust_data)
+
+                elif type_of_plot == 'bar': 
+                    cust_data = df[selected_columns_names]
+                    st.bar_chart(cust_data)
+
+                elif type_of_plot == 'line':
+                    cust_data = df[selected_columns_names]
+                    st.line_chart(cust_data)
+
+                # Custom Plot 
+                elif type_of_plot:
+                    cust_plot= df[selected_columns_names].plot(kind=type_of_plot)
+                    st.write(cust_plot)
+                    st.pyplot()
+    elif choice == 'Pre-Processing':
+        st.subheader("Different Pre-Processing Techniques")
+        data = st.file_uploader("Upload a Dataset", type=["csv", "txt", "xlsx"])
+        if data is not None:
+            df = pd.read_csv(data)
+            st.dataframe(df.head())
+            if st.checkbox("Label Encoding"):
+                all_columns = df.columns.to_list()
+                column_to_encode = st.selectbox("Select 1 Column",all_columns)
+                le = preprocessing.LabelEncoder() 
+                df[column_to_encode]= le.fit_transform(df[column_to_encode])
+                st.dataframe(df.head())
+            if st.checkbox("Standardization"):
+                all_columns = df.columns.to_list()
+                column= st.selectbox("Select 1 Column",all_columns)
+                df[column]= scaler1.fit_transform((df[column]).values.reshape(-1,1))
+                st.dataframe(df.head())
+            if st.checkbox("Min-Max Scaling"):
+                all_columns = df.columns.to_list()
+                column= st.selectbox("Select 1 Column",all_columns)
+                df[column]= scaler.fit_transform((df[column]).values.reshape(-1,1))
+                st.dataframe(df.head())
+
+
+if __name__ == '__main__':
+    main()
